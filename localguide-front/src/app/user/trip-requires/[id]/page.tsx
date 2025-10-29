569eda1e979bc84420d5dd9d9c6e1e6f3d9cf131
AceKamolwit <kamolwit.work@gmail.com>
2025-10-29 20:45:08 +0700

    ขยับ รายละเอียดไปด้านขวาสุด

--- localguide-front/src/app/user/trip-requires/[id]/page.tsx
+++ localguide-front/src/app/user/trip-requires/[id]/page.tsx
@@ -227,11 +227,16 @@ export default function TripRequireDetailPage() {
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           {/* Main Content */}
-          <TripRequireDetails tripRequire={tripRequire} />
+          <div className="lg:col-span-2">
+            <TripRequireDetails tripRequire={tripRequire} />
+          </div>
 
           {/* Sidebar */}
-          <TripRequireSidebar tripRequire={tripRequire} />
+          <div className="lg:col-start-3">
+            <TripRequireSidebar tripRequire={tripRequire} />
+          </div>
         </div>
+
 
         {/* Mobile Action Buttons */}
         <MobileActionButtons
